3a5d0d79af64c24f3baddc031672df533420497f
Edgar Fernandes <edgarfernandes@ipvc.pt>
2022-07-05 17:05:49 +0100

    Kill Feature

---
+++ src/trabalhopratico/JanelaController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhopratico;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import static javafx.scene.media.MediaPlayer.Status.PLAYING;
+import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+
+public class JanelaController implements Initializable {
+    
+    
+    private MediaView mv;
+     
+     MediaPlayer mediaplayer;
+    @FXML
+    private Pane root;
+  
+ 
+
+     
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+        
+    }    
+    
+    
+}
